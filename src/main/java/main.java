f9aabc0fbd0d4c8864ffd79e3befea43454f6262
staniukiewicz <staniukiewicz@gmail.com>
2019-03-02 10:51:32 +0100

    Commit 2

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n ; i++) {
+            System.out.println(i);
+        }
+
     }
 }
